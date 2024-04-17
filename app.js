91ba49b7be8134f1807fd892cdfc08d54dc76faf
nicocro <ncroce00@gmail.com>
2024-04-17 08:58:00 +0100

    cleaned up code a bit, and prepared for next things to add

--- app.js
+++ app.js
@@ -19,20 +19,32 @@ function Book(title, author, pages, read) {
 // add the book object to the book array
 // show the entire array on the page (adding cards HTML divs)
 
-function addBookToLibrary() {
-//function to 
+function addBookToLibrary(event) {
+//function to add a book to the library array and display it // 
+    event.preventDefault();
+
+    const title = document.getElementById('bookTitle').value;
+    const author = document.getElementById('bookAuthor').value;
+    const pages = parseInt(document.getElementById('numberOfPages').value);
+    const read = document.getElementById('readCheckbox').checked;
+
+    const newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+    displayBook()
 }
 
 function displayBook() {
-    
+    // traverse the library array and display all the books on the page //
     const library = document.querySelector('.library');
 
     library.innerHTML = '';
 
-    myLibrary.foreach(book => {
+    myLibrary.forEach((book, index) => {
         // function to create and display cards for every book in the myLibrary array //
         const bookCard  = document.createElement('div');
         bookCard.className = 'book-card';
+        bookCard.setAttribute('id',index);
 
         const title = document.createElement('h3');
         title.className = 'book-titile';
@@ -49,31 +61,52 @@ function displayBook() {
         pages.textContent = book.pages;
         bookCard.appendChild(pages);
 
+        const bookActions = document.createElement('div');
+        bookActions.className = "book-actions";
+
+        const btnDelete = document.createElement('button');
+        btnDelete.textContent = "Delete";
+        btnDelete.className = "delete-btn";
+        btnDelete.setAttribute('id', 'delete-' + index);  // Setting ID with index
+        
+
+        bookActions.appendChild(btnDelete);
+        bookCard.appendChild(bookActions);
+
         library.appendChild(bookCard);
     })
 
 }
 
-// Get the modal
-let modal = document.querySelector('#bookModal');
+function deleteBook(index) {
+// function to delete a book, triggered by eventlistener // 
+}
 
-// Get the button that opens the modal
-let btn = document.querySelector('#add-book');
 
-// Get the <span> element that closes the modal
-let xmodal = document.querySelector('.close');
+// Get the DOM elements I need
+const modal = document.querySelector('#bookModal');
+const btn = document.querySelector('#add-book');
+const xmodal = document.querySelector('.close');
 
-// When the user clicks the button, open the modal 
+// here I need to grab all the delete buttons and then add an eventlistener to all those buttons that applies the delete function. //
+
+// Open the modal on click of button //
 btn.addEventListener('click', function() {
     modal.style.display = "flex";
 });
 
+//close the modal by clicking the x //
 xmodal.addEventListener('click', function() {
     modal.style.display = "none";
 });
 
+//close the modal by clicking outside of it // 
 window.addEventListener('click', function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 });
+
+const bookForm = document.querySelector('#bookForm');
+
+bookForm.addEventListener('submit', addBookToLibrary);
